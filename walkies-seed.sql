ae9d669259db80ac6ec5c38a4db1e3324bfc0be0
Your Name <you@example.com>
2023-05-20 17:57:47 -0700

    added seed data for users, owners, walkers, jobs

--- walkies-seed.sql
+++ walkies-seed.sql
@@ -1,4 +1,4 @@
--- password is password
+-- password: {admin:password, johnnybravo:hairgel, jellyfishking:jellyfish}
 
 INSERT INTO users (email, first_name, last_name, username, password, role, is_admin, city, state, zipcode)
 VALUES ('admin@gmail.com',
@@ -11,16 +11,44 @@ VALUES ('admin@gmail.com',
         'san jose',
         'california',
         95123
-);
+        ),
+        (
+        'johnnybravo@gmail.com',
+        'johnny',
+        'bravo',
+        'johnnybravo',
+        '$2b$12$OipouLcD106whl7v51ME8ueV/Rkb1uSX81vYho.sLDbvDHmAcK6e.',
+        'dog walker',
+        FALSE,
+        'san jose',
+        'california',
+        95123
+        ),
+        ('jellyfishking@gmail.com',
+        'spongebob',
+        'squarepants',
+        'jellyfishking',
+        '$2b$12$IF28kwMiqOeOt7cJw7WbK.JpYG4Vvpt1qhfQNJgNLeeRoBMX8YWwC',
+        'dog owner',
+        FALSE,
+        'san jose',
+        'california',
+        95123
+        );
 
 
 INSERT INTO owners (bio, user_id)
-VALUES ('I am a Yorkie dad!',
-        1);
+VALUES ('I am a Yorkie dad!',1),
+       ('I am a Pug dad!',3);
+
+INSERT INTO walkers (user_id)
+VALUES (1);
 
 
 INSERT INTO jobs (date_of_walk, time_of_walk, pet_ids, owner_id, status)
-VALUES ('7/4/2023', '2:00 pm', '4,6,33', 1, 'available'),
-       ('7/5/2023', '2:00 pm', '4,6,33', 1, 'available');
+VALUES ('7/4/2023', '2:00 pm', '1,2,33', 1, 'available'),
+       ('7/5/2023', '2:00 pm', '1,2,33', 1, 'available'),
+       ('7/4/2023', '5:00 pm', '4,5,66', 2, 'available'),
+       ('7/5/2023', '5:00 pm', '4,5,66', 2, 'available');
 
 
