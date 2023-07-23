8565704d250e5176d4213ddf7aae95df4e8fe816
Your Name <you@example.com>
2023-07-22 23:28:44 -0700

    walkies.sql cleanup

--- walkies.sql
+++ walkies.sql
@@ -1,17 +1,17 @@
-\echo 'Delete and recreate walkies db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+-- \echo 'Delete and recreate walkies db?'
+-- \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE walkies;
+DROP DATABASE IF EXISTS walkies;
 CREATE DATABASE walkies;
 \connect walkies
 
 \i walkies-schema.sql
 \i walkies-seed.sql
 
-\echo 'Delete and recreate walkies_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+-- \echo 'Delete and recreate walkies_test db?'
+-- \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE walkies_test;
+DROP DATABASE IF EXISTS walkies_test;
 CREATE DATABASE walkies_test;
 \connect walkies_test
 
